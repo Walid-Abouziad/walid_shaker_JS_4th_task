7eab05b70c8e21ed0422e7d2134c600a83e8d01d
Walid Shaker <walidsh@gmail.com>
2023-12-27 02:13:25 +0400

    update function

--- js/home.js
+++ js/home.js
@@ -1,5 +1,9 @@
-
+if (sessionStorage.getItem("userName") == null){
+    window.location.assign("./index.html");
+}else
 document.getElementById("username").innerHTML = "Welcome "  + sessionStorage.getItem("userName");
 function logout(){
+    window.location.assign("./index.html");
     sessionStorage.removeItem('userName')
+
 }
